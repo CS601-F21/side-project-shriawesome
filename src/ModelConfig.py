c97e10ac2a7dfa2e96e255479a0d13c6614e2b6f
Shri <siddd.shriii@gmail.com>
2021-11-18 11:24:20 -0800

    Adding config file for constants

---
+++ src/ModelConfig.py
@@ -0,0 +1,9 @@
+class ModelConfig:
+    # File location for generating the dataset
+    DATA_CONFIG_FILE = "../dataset/metadata/lines.txt"
+
+    # Maximum text size for the input text
+    MAXTEXTLEN = 100 
+
+    #  Batch size for processing
+    BATCH_SIZE = 10
