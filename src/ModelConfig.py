39c27d0193df53a9e0d8ff8d07e6b36d2dcebf63
Shri <siddd.shriii@gmail.com>
2021-11-18 11:51:32 -0800

    Renaming a Variable Name

--- src/ModelConfig.py
+++ src/ModelConfig.py
@@ -3,7 +3,7 @@ class ModelConfig:
     DATA_CONFIG_FILE = "../dataset/metadata/lines.txt"
 
     # Maximum text size for the input text
-    MAXTEXTLEN = 100 
+    MAX_TEXT_LEN = 100
 
     #  Batch size for processing
     BATCH_SIZE = 10
