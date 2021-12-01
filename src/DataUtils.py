0573c87540b8864ee9eb358eeacf199ebe93583b
Shri <siddd.shriii@gmail.com>
2021-12-01 15:42:13 -0800

    Code cleaning

--- src/DataUtils.py
+++ src/DataUtils.py
@@ -1,10 +1,10 @@
 import numpy as np
 import tensorflow as tf
 import cv2
-# from tensorflow._api.v2 import image
+
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-# from tensorflow.python.ops.gen_array_ops import pad
-# from tensorflow.python.ops.gen_math_ops import Mod
+from tensorflow.keras.layers.experimental.preprocessing import StringLookup
+
 
 from ModelConfig import ModelConfig
 """
@@ -14,6 +14,8 @@ class DataUtils:
 
     def __init__(self,charList):
         self.charList = charList
+        self.char_to_num = StringLookup(vocabulary=list(self.charList), mask_token=None)
+        self.num_to_char = StringLookup(vocabulary=self.char_to_num.get_vocabulary(), mask_token=None, invert=True)
         tf.compat.v1.enable_eager_execution()
     
     """
@@ -63,43 +65,22 @@ class DataUtils:
         for alpha in txt:
             labelStr.append(self.charList.index(alpha))
 
-        return pad_sequences([labelStr],maxlen=ModelConfig.MAX_TEXT_LEN,padding="post",value=len(self.charList))[0]
+        return pad_sequences([labelStr],maxlen=ModelConfig.MAX_TEXT_LEN,padding="post",value=ModelConfig.PAD_TOKEN)[0]
 
     def numToChar(self,indices):
         txt=[]
         for idx in indices:
-            if idx<len(self.charList):
+            if idx<len(self.charList) and idx>=0:
                 txt.append(self.charList[idx])
         return("".join(txt))
 
-    
+
     def resizeImg(self,img):
         w, h = ModelConfig.IMG_WIDTH, ModelConfig.IMG_HEIGHT
-        img = tf.image.resize(img,[h,w])
-
-        # Padding to be done
-        padHeight = h - tf.shape(img)[0]
-        padWidth = w  - tf.shape(img)[1]
-
-        # Padding on both the ends, if even the same amount of padding on top and
-        # bottom else more on top and less on bottom
-
-        # if padWidth%2 != 0:
-        #     width = padWidth // 2
-        #     padWidthLeft = width + 1
-        #     padWidthRight = width
-        # else:
-        #     padWidthLeft = padWidthRight = padWidth // 2
-
-        # img = tf.pad(img,paddings=[
-        #     [padHeightTop,padHeightBottom],
-        #     [padWidthLeft,padWidthRight],
-        #     [0,0]
-        # ],)
-
+        img = tf.image.resize(img,size=(h,w))
         img = tf.transpose(img, perm=[1,0,2])
         img = tf.image.flip_left_right(img)
-        
+    
         return img
 
 
@@ -108,14 +89,10 @@ class DataUtils:
         # Read the input file
         img = tf.io.read_file(imgPath)
         img = tf.io.decode_png(img,1)
-        # scalling the image between [0,1]
-        img = tf.image.convert_image_dtype(img, tf.float32)
     
-        # Rescaling the image to 64,800    
+        # Rescaling the image to 64,800 
+        img = tf.image.convert_image_dtype(img, tf.float32)   
         img = self.resizeImg(img)
-        # print(img.shape)
-        #cv2.imshow("",np.array(img))
-        #ccv2.waitKey(0)
 
         return {"image":img,"label":Label}
 
@@ -130,11 +107,7 @@ class DataUtils:
             validationData = validationData.map(self.processSingleSample,num_parallel_calls=tf.data.AUTOTUNE).batch(ModelConfig.BATCH_SIZE).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
         except:
             print("Error creating a pipeline")
-        
-        # encoding the image in a numpy array
-       # trainData = trainData.map(self.processSingleSample).batch(ModelConfig.BATCH_SIZE).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-        
+    
         # Creating pipleline for validation data
        
-        print("'IN")
         return trainData, validationData
