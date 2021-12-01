fa9fd7301fdd944fed1d6a34a88750ae4e0a912c
Shri <siddd.shriii@gmail.com>
2021-12-01 15:17:08 -0800

    Chaning constant values to optimised values after model testing and evaluation

--- src/ModelConfig.py
+++ src/ModelConfig.py
@@ -1,21 +1,24 @@
 class ModelConfig:
     # File location for generating the dataset
-    DATA_CONFIG_FILE = "../dataset/metadata/lines.txt"
+    DATA_CONFIG_FILE = "../dataset/metadata/words.txt"
 
     # Maximum text size for the input text
-    MAX_TEXT_LEN = 100
+    MAX_TEXT_LEN = 21
+
+    # Padding Token
+    PAD_TOKEN = 99
 
     # Batch size for processing
     BATCH_SIZE = 64
 
     # Image widht and height
-    IMG_WIDTH = 800
-    IMG_HEIGHT = 64
+    IMG_WIDTH = 128
+    IMG_HEIGHT = 32
 
     # Epochs for training
     EPOCHS = 10
     EARLY_STOP_PATIENCE = 10
 
     # MODEL NAME TO BE SAVED
-    SAVE_MODEL = "../models/HWRModel_v1.h5"
+    SAVE_MODEL = "../models/HWRModel_v4.h5"
 
