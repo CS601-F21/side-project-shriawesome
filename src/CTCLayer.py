52996e5df4c70af27b0a747790aa8adf391e6bdf
Shri <siddd.shriii@gmail.com>
2021-12-01 15:16:02 -0800

    Completing loss evaluation

--- src/CTCLayer.py
+++ src/CTCLayer.py
@@ -3,6 +3,9 @@ from tensorflow import keras
 import tensorflow as tf
 from tensorflow.python.keras.backend import dtype
 
+"""
+Connectionist Temporal Classification loss for training the model
+"""
 class CTCLayer(layers.Layer):
 
     def __init__(self,name=None,**kwargs):
@@ -11,15 +14,18 @@ class CTCLayer(layers.Layer):
         # self.ignore_longer_outputs_than_inputs=True
         super(CTCLayer,self).__init__(**kwargs)
 
-    def call(self,yTrue,yPred):
-        batchLen = tf.cast(tf.shape(yTrue)[0],dtype='int64')
-        inputLength = tf.cast(tf.shape(yPred)[1],dtype='int64')
-        labelLength = tf.cast(tf.shape(yTrue)[1],dtype='int64')
+    """
+    Ref : https://keras.io/examples/vision/handwriting_recognition/
+    """
+    def call(self, y_true, y_pred):
+        batchLen = tf.cast(tf.shape(y_true)[0], dtype="int64")
+        inputLength = tf.cast(tf.shape(y_pred)[1], dtype="int64")
+        labelLength = tf.cast(tf.shape(y_true)[1], dtype="int64")
 
-        inputLength = inputLength * tf.ones(shape=(batchLen,1),dtype='int64')
-        labelLength = labelLength * tf.ones(shape=(batchLen,1),dtype='int64')
-
-        loss = self.loss_fn(yTrue,yPred,inputLength,labelLength)
+        inputLength = inputLength * tf.ones(shape=(batchLen, 1), dtype="int64")
+        labelLength = labelLength * tf.ones(shape=(batchLen, 1), dtype="int64")
+        loss = self.loss_fn(y_true, y_pred, inputLength, labelLength)
         self.add_loss(loss)
 
-        return yPred
+        # At test time, just return the computed predictions.
+        return y_pred
